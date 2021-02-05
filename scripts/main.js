ff805abaaf42dbc328b84848c5e52d0ce7d6ce41
Juan Zaragoza <56614909+ItsJuanito@users.noreply.github.com>
2021-02-05 00:26:14 -0800

    Update main.js
    Fixed function indentation.

--- scripts/main.js
+++ scripts/main.js
@@ -60,10 +60,10 @@ function page_update(info) {
 }
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
